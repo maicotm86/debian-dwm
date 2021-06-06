6d5550f33da0eda0160805af93d5f66893a15c23
maicotm <maicotedesco@gmail.com>
2021-06-06 13:46:26 -0300

    pgAdmin4 add

--- debian.sh
+++ debian.sh
@@ -15,6 +15,35 @@ sudo apt update -yy
 
 sudo apt install brave-browser -yy
 
+#
+# Setup the repository
+#
+
+# Install the public key for the repository (if not done previously):
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+
+# Create the repository configuration file:
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+
+#
+# Install pgAdmin
+#
+
+# Repo update
+sudo apt update -yy
+
+# Install for both desktop and web modes:
+sudo apt install pgadmin4 -yy
+
+# Install for desktop mode only:
+sudo apt install pgadmin4-desktop -yy
+
+# Install for web mode only:
+sudo apt install pgadmin4-web  -yy
+
+# Configure the webserver, if you installed pgadmin4-web:
+sudo /usr/pgadmin4/bin/setup-web.sh
+
 cd
 
 mkdir -p .local/bin
